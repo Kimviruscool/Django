f6a2a8049a78775b997b31b24a3f97ff71d82a43
Kimviruscool <sinsa9122@naver.com>
2025-11-26 14:03:00 +0900

    Initial commit

---
+++ day1/가상환경.py
@@ -0,0 +1,12 @@
+#장고 설치하기
+# terminal에서 pip install django 입력하여 django 설치
+# django-admin startproject mysite. #django-관리자 startproject(프로젝트시작) mysite(프로젝트명).(.은 현재 내가 위치하는 폴더에 풀겠다)
+# cd mysite #cd 이동하겠다 mysite(프로젝트명)으로
+# python manage.py runserver #파이썬에서 manage.py(실행파일) runserver(서버배포실행을하겠다)
+# Ctrl + C 를 눌러 콘솔에서 빠져나온다.
+
+#sPybo앱 생성하기
+# django-admin startapp pybo
+# mysite > urls.py > import & path(adress)
+
+#P.44까지 진행됨
